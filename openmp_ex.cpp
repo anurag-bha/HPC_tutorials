fa3e1c5616173286d129223e04719ddef1a7e033
Ubuntu <ubuntu@ip-172-31-2-179.us-west-1.compute.internal>
2024-09-06 22:58:32 +0000

    testing dumpirng output to text file

--- openmp_ex.cpp
+++ openmp_ex.cpp
@@ -1,3 +1,22 @@
 
+
 // This code teaches OpenMP 
 // This code is written in C++
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+
+	int a = 10;
+	int b = 20;
+	int val;
+
+	val = a+b;
+	cout << val;
+
+
+	return 0;
+
+}
